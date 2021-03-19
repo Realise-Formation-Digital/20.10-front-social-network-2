bed841751458c607ca48b4c8adb16d6c54cf9f15
Hugo <hllr.regadas@gmail.com>
2021-03-19 10:48:57 +0100

    Sidebar

---
+++ src/Components/Sidebar.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import Channel from './Channel'
+
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            <h5>CHAÎNES RECOMMANDÉES</h5>
+            <Channel avatar="https://static-cdn.jtvnw.net/jtv_user_pictures/7ed5e0c6-0191-4eef-8328-4af6e4ea5318-profile_image-70x70.png"
+                name="shroud"
+                followers="29,7 k" />
+            <Channel avatar="https://static-cdn.jtvnw.net/jtv_user_pictures/b70c8c41-be63-4eb6-b863-4328cc6e834f-profile_image-70x70.png"
+                name="Tev_et_Luis"
+                followers="9 k" />
+            <Channel avatar=""
+                name=""
+                followers="" />
+            <Channel avatar=""
+                name=""
+                followers="" />
+            <Channel avatar=""
+                name=""
+                followers="" />
+            <Channel avatar=""
+                name=""
+                followers="" />
+            <Channel avatar=""
+                name=""
+                followers="" />
+            <Channel avatar=""
+                name=""
+                followers="" />
+            <Channel avatar=""
+                name=""
+                followers="" />
+
+        </div>
+    )
+}
+
+export default Sidebar
